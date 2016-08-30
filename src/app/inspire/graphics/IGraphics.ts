e4e453cf3e704f3dc0ae4be81c891d927b8632e1
Harald Luebeck <harald@harald-luebeck.de>
2016-08-30 13:26:54 +0200

    Add Graphics Interface

---
+++ src/app/inspire/graphics/IGraphics.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by Harald on 29/08/2016.
+ */
+
+namespace inspire.graphics
+{
+
+    export interface IGraphics
+    {
+
+
+
+    }
+
+
+}
---
+++ src/app/inspire/graphics/canvas/GraphicsImpl.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Harald on 29/08/2016.
+ */
+
+namespace inspire.graphics.canvas
+{
+    export class CanvasGraphics implements IGraphics
+    {
+    }
+}
