84c4f590c7419e105de07dc9fbc97f230f3431d0
Oleg Varaksin <ovaraksin@googlemail.com>
2016-09-19 09:04:00 +0200

    Just for fun

--- src/app/graphics/SVGGraphicContext.ts
+++ src/app/graphics/SVGGraphicContext.ts
@@ -39,47 +39,34 @@ export class SVGGraphicContext extends DrawingEngine
 
 	private drawGrid(): void
 	{
-		let stepx = 10;
-		let stepy = 10;
-
-
-		let pathPoints:string = "";
-
-		for (let i = stepx + 0.5; i < this.viewPort.width; i += stepx)
-		{
-			pathPoints = pathPoints +  `M ${i} 0 L ${i} ${this.viewPort.height} `;
-
-		}
-
-		for (let i = stepy + 0.5; i < this.viewPort.height; i += stepy)
-		{
-			pathPoints = pathPoints +  `M 0 ${i} L ${this.viewPort.height} ${i}  `;
-		}
-
-
-
-
-		console.log( "Path: " + pathPoints);
-		let pathElement:Snap.Element = this._snapSVG.path( pathPoints );
-
-		pathElement.attr(
-		{
-			stroke: "#000",
-			strokeWidth: 1
-		});
-
-
-
-
+        let stepx = 10;
+        let stepy = 10;
+          let width = this.viewPort.width;
+          let height = this.viewPort.height;
+          
+        let path: string = '';
+  
+        for (let i = stepx + 0.5; i < width; i += stepx)
+        {
+            path = path +  `M${i},0L${i},${height}`;
+        }
+  
+        for (let i = stepy + 0.5; i < height; i += stepy)
+        {
+            path = path +  `M0,${i}L${width},${i}`;
+        }
+        
+        let p = this._snapSVG.path(path);        
+        p.attr({
+            stroke: "#cccccc",
+            strokeWidth: 0.5
+        });
 	}
-
-
 
 	private _snapSVG: Snap.Paper;
 
 	public draw(nodeElement: NodeElement): void
 	{
-
 		let svgElement:Snap.Element = null;
 		console.log("Draw: " + (nodeElement == null ? " NONE " : nodeElement.getName()));
 
@@ -116,6 +103,31 @@ export class SVGGraphicContext extends DrawingEngine
 		{
 			this.internalDraw(nodeElement);
 		}
+		
+        // just for fun ======================================
+        this._snapSVG.clear();
+        this.drawGrid();
+        
+        let circle = this._snapSVG.circle(100, 100, 25);
+        circle.attr({
+           fill: "r()#e4e4d9-#215f00",
+           stroke: "#000000", 
+           strokeWidth: 1
+        });
+        
+        let rect = this._snapSVG.rect(200, 40, 110, 80, 6, 6);
+        rect.attr({
+           fill: "r()#d2ddfc-#a099e8", 
+           stroke: "#000000", 
+           strokeWidth: 1
+        });
+        
+        let t1 = this._snapSVG.text(255, 80, "Task 1");
+        t1.attr({"alignmentBaseline": "middle", "textAnchor": "middle", "fontSize": 14});
+        
+        let t2 = this._snapSVG.text(100, 145, "Start 1");
+        t2.attr({"alignmentBaseline": "middle", "textAnchor": "middle", "fontSize": 14});
+        // ===================================================
 
 	}
 
